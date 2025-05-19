0c72fc68a55d9c942eb318b89fe63c35afd6b82a
Maksym <maxpavliy@gmail.com>
2025-05-19 15:15:49 +0200

    added error handling for crypto endpoint

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 from typing import List
 from sqlalchemy.orm import sessionmaker
 from database.db import db
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 from models.models import User
 
@@ -27,17 +27,34 @@ def get_all_users() -> List[UserType]:
 async def read_item(item_id):
     return {"item_id": item_id}
 
-@app.get("/crypto/{crypto_name}")
-async def get_crypto(crypto_name): 
+@app.get("/crypto/{crypto_name}", status_code=200)
+async def get_crypto(crypto_name, res: Response): 
     cg = CoinGeckoAPI(demo_api_key=os.getenv('GECKO_API_KEY'))
 
-    data = cg.get_price(ids='bitcoin', vs_currencies='usd')
+    try:
+        data = cg.get_price(ids=crypto_name, vs_currencies='usd')
 
-    price = data['bitcoin']['usd']
+        price = data[f'{crypto_name}']['usd']
 
-    formatted =f"{price:,}".replace(",", " ")
+        formatted =f"{price:,}".replace(",", " ")
 
-    return {
-        "data": data,
-        "price": formatted
-    }
+        res.status_code = 200
+
+        return {
+            "data": data,
+            "price": formatted
+        }
+    except:
+        res.status_code = 404
+
+        return {
+            "Message": "Incorrect crypto",
+        }
+
+@app.post("/auth/register")
+async def register():
+    pass
+
+@app.post("/auth/login")
+async def login():
+    pass
